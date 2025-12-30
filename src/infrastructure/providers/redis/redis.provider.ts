c0027c93ba4085d5fcf4d813f4e870179ac2e3ff
Anshida-Ansari <anshidaansari9a@gmail.com>
2025-12-30 19:37:34 +0530

    load env variables before redis to fix NaN port error

--- src/infrastructure/providers/redis/redis.provider.ts
+++ src/infrastructure/providers/redis/redis.provider.ts
@@ -1,8 +1,5 @@
 import Redis from 'ioredis'
 import 'dotenv/config'
-console.log('REDIS_HOST:', process.env.REDIS_HOST);
-console.log('REDIS_PORT:', process.env.REDIS_PORT);
-console.log('REDIS_PASSWORD:', process.env.REDIS_PASSWORD);
 
 export const redisClient = new Redis({
     host:process.env.REDIS_HOST, 
