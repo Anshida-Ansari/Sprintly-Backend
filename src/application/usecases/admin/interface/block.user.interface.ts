619de4b20f11351272b734bc2440c5af5ea6222e
Anshida-Ansari <anshidaansari9a@gmail.com>
2026-01-07 15:57:23 +0530

    feat(auth): revoke access immediately for block users using redis

--- src/application/usecases/admin/interface/block.user.interface.ts
+++ src/application/usecases/admin/interface/block.user.interface.ts
@@ -1,4 +1,4 @@
-import { UserStatus } from "../../../../domain/enum/status.enum";
+import { UserStatus } from "@domain/enum/status.enum";
 
 export interface IBlockUserUseCase {
     execute(userId: string, status: UserStatus): Promise<{ message: string }>;
